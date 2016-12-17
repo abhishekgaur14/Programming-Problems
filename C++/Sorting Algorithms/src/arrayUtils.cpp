1cd309d59be0f5e302a790631475e8018e97f000
abhishekgaur14 <abhigaur@bu.edu>
2016-12-17 12:22:17 -0500

    Added Quicksort to the Sorting Algorithms

---
+++ C++/Sorting Algorithms/src/arrayUtils.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "arrayUtils.h"
+
+using namespace std;
+
+void printArray(int *arr, int size)
+{
+	for(int i=0;i<size;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	cout<<endl;
+}
